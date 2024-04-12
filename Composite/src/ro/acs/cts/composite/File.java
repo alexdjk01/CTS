69378f6eae9f78f2176d9376689a800dbf233767
Alexandru Mihai Ionel <ionelalexandru01@gmail.com>
2024-04-12 12:15:25 +0300

    adapter composite

---
+++ Composite/src/ro/acs/cts/composite/File.java
@@ -0,0 +1,16 @@
+package ro.acs.cts.composite;
+
+public class File implements FileSystemItem {
+
+	private String name;
+
+    public File(String name){
+        this.name=name;
+    }
+    @Override
+    public void printName() {
+        System.out.println("File:   " + this.name);
+    }
+ 
+	
+}
